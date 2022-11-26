ec75dd3e4cfc44c5b54876c5b7f7af2102bfd188
Alexey Paramonov <paramonov_tama@mail.ru>
2022-11-26 19:16:27 +0300

    feat: button and inputText in index.xhtml

---
+++ src/main/java/by/paramonov/linkcount/Link.java
@@ -0,0 +1,13 @@
+package by.paramonov.linkcount;
+
+public class Link {
+    private String linkName;
+
+    public String getLinkName() {
+        return linkName;
+    }
+
+    public void setLinkName(String linkName) {
+        this.linkName = linkName;
+    }
+}
---
+++ src/main/java/by/paramonov/linkcount/LinkCountApp.java
@@ -0,0 +1,7 @@
+package by.paramonov.linkcount;
+
+import org.primefaces.PrimeFaces;
+
+public class LinkCountApp {
+    PrimeFaces pfInstance = PrimeFaces.current();
+}
