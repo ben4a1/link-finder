f6819c74135324c0fd71c6d8d79d4175a23cbef7
Alexey Paramonov <paramonov_tama@mail.ru>
2022-11-27 15:25:21 +0300

    fix: Set -> Map in 'JsoupFindLink'

--- src/main/java/by/paramonov/linkcount/controller/JsoupFindLink.java
+++ src/main/java/by/paramonov/linkcount/controller/JsoupFindLink.java
@@ -7,33 +7,36 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class JsoupFindLink {
 
     public static void main(String[] args) throws IOException {
         Link link = new Link("https://github.com/");
-        Set<String> links = findLinks(link.getUrl());
+        Map<String, String> links = findLinks(link.getUrl());
         System.out.println(links);
     }
 
-    public static Set<String> findLinks(String url) throws IOException {
-        Set<String> links = new HashSet<>();
+    public static Map<String, String> findLinks(String url) throws IOException {
+        Map<String, String> links = new HashMap<>();
         Document doc = Jsoup.connect(url)
                 .data("query", "Java")
                 .userAgent("Chrome")
                 .cookie("auth", "token")
                 .timeout(3000)
                 .get();
-        System.out.println(doc.title());
 
 
         Elements elements = doc.select("a[href]");
         for (Element element : elements) {
-            if (!element.attr("href").startsWith("https://")) {
-                links.add(element.attr(url + "href"));
-            } else links.add(element.attr("href"));
+
+            if (element.attr("href").startsWith("https://")) {
+                links.put(element.text(),
+                        element.attr("href"));
+            }
         }
         return links;
     }
